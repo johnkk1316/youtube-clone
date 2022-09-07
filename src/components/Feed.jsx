4f4ce88c59e2e9bc4ed9ad9811fc16be1bfdd350
John KK <johnkk1316@protonmail.com>
2022-09-07 19:06:41 +0300

    commenced on the new video sector and also imported videos component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
 
-import Sidebar from './Sidebar'
+import { Sidebar, Videos } from './'
 
 const Feed = () => {
     return (
@@ -12,7 +12,15 @@ const Feed = () => {
             <Typography className="copyright" variant="body2" sx={{mt: 1.5, color: '#fff'}}>
                 Copyright 2022 Codeona Media
             </Typography>
-        </Box> 
+        </Box>
+
+        <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+            <Typography variant="h4" fontWeight="bold" mb={2} sx={{color: 'white',}}>
+                NEW <span style={{ color: '#F31503' }} >videos</span>
+            </Typography>
+
+            <Videos videos={{}} />   
+        </Box>
     </Stack>
     )
 }
