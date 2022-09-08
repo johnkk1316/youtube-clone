bec882a3bc21d5c679f9c4527b38630d27b75741
John KK <johnkk1316@protonmail.com>
2022-09-08 16:04:25 +0300

    implemented channel panel(profile page.)

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import{ Videos, ChannelCard } from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const ChannelDetail = () => {
+    const [channelDetail, setChannelDetail] = useState(null)
+    const [videos, setVideos] = useState(null)
+
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetchFromAPI(`channels?part=snippet&id=${id}`)
+            .then((data) => setChannelDetail(data?.items[0]));
+            
+        fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+            .then((data) => setVideos(data?.items));
+    }, [id])
+
     return (
-        <div>ChannelDetail</div>
+        <Box minHeight="95vh">
+            <Box>
+                <div style={{
+                    background: 'radial-gradient(circle, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+                    zIndex: 10,
+                    height: '300px',
+                }} 
+                />
+                    <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+            </Box>
+            <Box display="flex" p="2">
+                <Box sx={{ mr: { sm: '100px' }}} />
+                <Videos videos={videos} />         
+            </Box>
+        </Box>
     )
 }
 
